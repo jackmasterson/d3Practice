db4112ed278b6d831efdac86995301cefd6327dc
JackMasterson <jackmasterson5@gmail.com>
2016-08-03 20:06:08 -0400

    set properties as function returns

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,17 @@
 var dataset = [ 5, 10, 15, 20, 25 ];
+
+		d3.select("body").selectAll("p")
+		    .data(dataset)
+		    .enter()
+		    .append("p")
+		    .text(function(d) {
+		    	return d;
+		    })
+		    .style('color', function(d){
+		    	if(d > 15) {
+		    		return 'red';
+		    	}
+		    	else {
+		    		return 'blue';
+		    	}
+		    });
